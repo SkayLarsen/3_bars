1f474c9613040c774d1217f2ad0398b8a87d3fe8
Скай Ларсен <SkayLarsen@users.noreply.github.com>
2017-01-04 12:51:19 +0400

    some fixes

--- bars.py
+++ bars.py
@@ -6,11 +6,10 @@ import math
 
 
 def load_data(filepath):
-    if os.path.exists(filepath):
-        with codecs.open(filepath, "r", "cp1251") as data_file:
-            return json.loads(data_file.read())
-    else:
+    if not os.path.exists(filepath):
         return None
+    with codecs.open(filepath, "r", "cp1251") as data_file:
+        return json.loads(data_file.read())
 
 
 def get_biggest_bar(data):
@@ -23,24 +22,25 @@ def get_smallest_bar(data):
     return smallest_bar["Name"], smallest_bar["SeatsCount"]
 
 
-def circ_dist(long1, lat1, long2, lat2):
+def get_distance(long1, lat1, long2, lat2):
+    angular_distance_const = 6367444.6571225
     long1, lat1, long2, lat2 = map(math.radians, [long1, lat1, long2, lat2])
     sin1 = math.sin(lat1)
     sin2 = math.sin(lat2)
     cos1 = math.cos(lat1)
     cos2 = math.cos(lat2)
     cos12 = math.cos(long2 - long1)
-    return 6367444.6571225 * math.acos(sin1 * sin2 + cos1 * cos2 * cos12)
+    return angular_distance_const * math.acos(sin1 * sin2 + cos1 * cos2 * cos12)
 
 
 def get_closest_bar(data, longitude, latitude):
     long = float(longitude)
     lat = float(latitude)
-    closest_bar = min(data, key=lambda x: circ_dist(long, lat,
+    closest_bar = min(data, key=lambda x: get_distance(long, lat,
                                                     x["geoData"]["coordinates"][1],
                                                     x["geoData"]["coordinates"][0]))
     min_dist = round(
-        circ_dist(long, lat,
+        get_distance(long, lat,
                   closest_bar["geoData"]["coordinates"][1],
                   closest_bar["geoData"]["coordinates"][0]) / 1000, 2)
     return closest_bar["Name"], min_dist
